4e6830593bcc32484b7960d62283f4de59160645
Alexis Ccoñislla <108102006+alex-ccobregon@users.noreply.github.com>
2022-10-13 21:20:17 -0500

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,6 +1,19 @@
+//Swal.fire({
+//    title: 'Bienvenido!!',
+//    text: 'clickea fuera del cuadro para continuar...',
+//    backdrop: 'true',
+//    showConfirmButton: false
+//});
 Swal.fire({
+    icon: 'warning',
     title: 'Bienvenido!!',
-    text: 'clickea fuera del cuadro para continuar...',
-    backdrop: 'true',
-    showConfirmButton: false
-});
+    input: 'checkbox',
+    inputValue: 1,
+    inputPlaceholder:
+      'Acepto las cookies para mejora de experiencia de usuario.🤖',
+    confirmButtonText:
+      'Continuar <i class="fa fa-arrow-right"></i>',
+    inputValidator: (result) => {
+      return !result && 'Si no aceptas no tendras la mejor experiencia que ofrecemos.😐'
+    }
+  })
